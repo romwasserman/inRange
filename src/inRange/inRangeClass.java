0a1766272c0ea7168e47c93b64fe9792e61eedeb
romwasserman <rom.wasserman@quinnipiac.edu>
2021-02-08 14:01:30 -0500

    added extra info about myself

--- src/inRange/inRangeClass.java
+++ src/inRange/inRangeClass.java
@@ -3,5 +3,6 @@ package inRange;
 public class inRangeClass {
 	public static void main(String[] args) {
 		System.out.println("My name is rom wasserman!");
+		System.out.println("I am from Tel Aviv,Israel!");
 	}
 }
