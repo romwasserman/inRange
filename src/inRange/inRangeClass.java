8edcab515c1a601fb69f09b0c4c4a9a49fe58037
romwasserman <rom.wasserman@quinnipiac.edu>
2021-02-08 14:05:28 -0500

    deleted the second println command

--- src/inRange/inRangeClass.java
+++ src/inRange/inRangeClass.java
@@ -3,6 +3,5 @@ package inRange;
 public class inRangeClass {
 	public static void main(String[] args) {
 		System.out.println("My name is rom wasserman!");
-		System.out.println("I play here for the QU men's soccer team!");
 	}
 }
