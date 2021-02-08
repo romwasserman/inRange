dc274fd2f382f33c36bddeb7156bf63714cbdcca
romwasserman <rom.wasserman@quinnipiac.edu>
2021-02-08 14:08:22 -0500

    added two println commands

--- src/inRange/inRangeClass.java
+++ src/inRange/inRangeClass.java
@@ -3,5 +3,7 @@ package inRange;
 public class inRangeClass {
 	public static void main(String[] args) {
 		System.out.println("My name is rom wasserman!");
+		System.out.println("My name is Rom Wasserman!");
+		System.out.println("I am 21 years old!");
 	}
 }
