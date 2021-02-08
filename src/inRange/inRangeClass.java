4015d8281701b71da7e2337912b44c8388c7ce75
romwasserman <rom.wasserman@quinnipiac.edu>
2021-02-08 14:04:20 -0500

    changed the println content

--- src/inRange/inRangeClass.java
+++ src/inRange/inRangeClass.java
@@ -3,6 +3,6 @@ package inRange;
 public class inRangeClass {
 	public static void main(String[] args) {
 		System.out.println("My name is rom wasserman!");
-		System.out.println("I am from Tel Aviv,Israel!");
+		System.out.println("I play here for the QU men's soccer team!");
 	}
 }
